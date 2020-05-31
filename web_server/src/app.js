47ec1245064d146df11477532f403968d082471d
prerna <bhatraprerna061@gmail.com>
2020-05-31 18:43:07 +0530

    partial

--- web_server/src/app.js
+++ web_server/src/app.js
@@ -1,18 +1,29 @@
 const path=require('path');
 const express=require('express');
+const hbs=require('hbs')
 console.log(__dirname);
 //console.log(__filename);
-const publicpath=path.join(__dirname,'../public')
 const app=express()
+const publicpath=path.join(__dirname,'../public')
+//const viewspath=path.join(__dirname,'../templates/views')
+const partialspath=path.join(__dirname,'./partial')
+app.set('view engine','hbs')
+//app.set('views',viewspath)
+hbs.registerPartials(partialspath)
 app.use(express.static(publicpath))
 app.get('',(req,res)=>
 {
 	res.send("hello express");
 })
 
+app.get('/help',(req,res)=>
+{
+	res.render('help');
+})
+
 //use hbs
 
 
 app.listen(3000,()=>{
-	console.log("server is us on posrt 3000");
+	console.log("server is  on port 3000");
 })
